d3d970775a04390609c68f1ab2a3b0af400725c3
Samuel Skånberg <samuel.skanberg@pingdom.com>
2015-01-19 15:21:33 +0100

    Changed frome privileged to web app

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 window.addEventListener("load", function() {
     console.log("Hello World!");
+
+    var canInstall = !!(navigator.mozApps && navigator.mozApps.installPackage);
+    if (canInstall) {
+        var manifestURL = "http://samuelskanberg.github.io/vecka/manifest.webapp";
+        var installApp = navigator.mozApps.installPackage(manifestURL);
+    }
+
     var timeUrl = "http://api.dryg.net/dagar/v1/";
 
     $.getJSON(timeUrl, function(data) {
