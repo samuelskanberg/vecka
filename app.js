7a3668f86f5e2e9d742f72ab266f8f52ac129438
Samuel Skånberg <samuel.skanberg@pingdom.com>
2015-01-21 09:15:15 +0100

    Checked if app is already installed before installing

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 window.addEventListener("load", function() {
-    console.log("Hello World!");
+    console.log("Vecka!");
 
-    // Install app
-    if (navigator.mozApps) {
-        var manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
-        var req = navigator.mozApps.install(manifestURL);
-        req.onsuccess = function() {
-            console.log(this.result.origin);
-        };
-        req.onerror = function() {
-            console.log(this.error.name);
-        };
-    }
+    var manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+    // Check if app is installed
+    var request = window.navigator.mozApps.checkInstalled(manifestURL);
+    request.onerror = function(e) {
+        alert("Error calling checkInstalled: " + request.error.name);
+    };
+    request.onsuccess = function(e) {
+        // If app is not installed, install it
+        if (!request.result) {
+            if (navigator.mozApps) {
+                console.log("Manifest url: "+manifestURL);
+                var req = navigator.mozApps.install(manifestURL);
+                req.onsuccess = function() {
+                    console.log(this.result.origin);
+                };
+                req.onerror = function() {
+                    console.log(this.error.name);
+                };
+            }
+        }
+    };
 
     // Get week number and set it
     var timeUrl = "http://api.dryg.net/dagar/v1/";
